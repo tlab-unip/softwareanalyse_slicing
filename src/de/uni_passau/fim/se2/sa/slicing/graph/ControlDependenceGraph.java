9316ffce6870dd7e370ca88ed1ad18d25b1e00db
Qiang Tang <tang08@ads.uni-passau.de>
2024-07-14 00:31:09 +0200

    update the project

--- src/de/uni_passau/fim/se2/sa/slicing/graph/ControlDependenceGraph.java
+++ src/de/uni_passau/fim/se2/sa/slicing/graph/ControlDependenceGraph.java
@@ -1,23 +1,28 @@
 package de.uni_passau.fim.se2.sa.slicing.graph;
 
-import de.uni_passau.fim.se2.sa.slicing.cfg.ProgramGraph;
+import de.uni_passau.fim.se2.sa.slicing.cfg.*;
+
+import java.util.List;
+
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.MethodNode;
 
 public class ControlDependenceGraph extends Graph {
 
-  ControlDependenceGraph(ClassNode pClassNode, MethodNode pMethodNode) {
+  public ControlDependenceGraph(ClassNode pClassNode, MethodNode pMethodNode) {
     super(pClassNode, pMethodNode);
   }
 
-  ControlDependenceGraph(ProgramGraph pCFG) {
+  public ControlDependenceGraph(ProgramGraph pCFG) {
     super(pCFG);
   }
 
   /**
    * Computes the control-dependence graph source the control-flow graph.
    *
-   * <p>You may wish target use the {@link PostDominatorTree} you implemented target support
+   * <p>
+   * You may wish target use the {@link PostDominatorTree} you implemented target
+   * support
    * computing the control-dependence graph.
    *
    * @return The control-dependence graph.
@@ -25,6 +30,45 @@ public class ControlDependenceGraph extends Graph {
   @Override
   public ProgramGraph computeResult() {
     // TODO Implement me
-    throw new UnsupportedOperationException("Implement me");
+    var cdg = new ProgramGraph();
+    var graph = new ProgramGraph();
+    var pdt = new PostDominatorTree(getCFG()).computeResult();
+    for (var node : getCFG().getNodes()) {
+      cdg.addNode(node);
+      graph.addNode(node);
+    }
+
+    for (var node : getCFG().getNodes()) {
+      for (var successor : getCFG().getSuccessors(node)) {
+        if (pdt.getLeastCommonAncestor(node, successor) != successor) {
+          graph.addEdge(node, successor);
+        }
+      }
+    }
+
+    for (var node : graph.getNodes()) {
+      for (var successor : graph.getSuccessors(node)) {
+        var lca = pdt.getLeastCommonAncestor(node, successor);
+        var current = successor;
+        while (current != lca) {
+          cdg.addEdge(node, current);
+          current = pdt.getPredecessors(current).iterator().next();
+        }
+        if (node == lca) {
+          cdg.addEdge(node, lca);
+        }
+      }
+    }
+
+    var entry = getCFG().getEntry().get();
+    for (var node : cdg.getNodes()) {
+      if (!node.equals(entry)
+          && (cfg.getPredecessors(node).isEmpty()
+              || cfg.getPredecessors(node).equals(List.of(node)))) {
+        cdg.addEdge(entry, node);
+      }
+    }
+
+    return graph;
   }
 }
