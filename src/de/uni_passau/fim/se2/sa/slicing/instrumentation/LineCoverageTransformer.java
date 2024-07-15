686a11f454a379555e108a743055f893c0749192
Qiang Tang <tang08@ads.uni-passau.de>
2024-07-15 21:30:03 +0200

    update the project

--- src/de/uni_passau/fim/se2/sa/slicing/instrumentation/LineCoverageTransformer.java
+++ src/de/uni_passau/fim/se2/sa/slicing/instrumentation/LineCoverageTransformer.java
@@ -3,6 +3,11 @@ package de.uni_passau.fim.se2.sa.slicing.instrumentation;
 import java.lang.instrument.ClassFileTransformer;
 import java.security.ProtectionDomain;
 
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+
 public class LineCoverageTransformer implements ClassFileTransformer {
 
   private final String instrumentationTarget;
@@ -24,6 +29,11 @@ public class LineCoverageTransformer implements ClassFileTransformer {
 
     // TODO Implement me
     try {
+      ClassReader reader = new ClassReader(pClassName);
+      ClassWriter writer = new ClassWriter(reader, ClassWriter.COMPUTE_FRAMES);
+      ClassVisitor visitor = new InstrumentationAdapter(Opcodes.ASM9, writer);
+      reader.accept(visitor, 0);
+      return writer.toByteArray();
     } catch (Exception e) {
     }
     return pClassFileBuffer;
